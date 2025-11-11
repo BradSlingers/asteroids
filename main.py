46c561770c2f02d13c47be12a9eca061a60f4428
BradSlingers <btslingers@gmail.com>
2025-11-11 08:06:20 +0200

    Deleted json file. Drew picture to the screen

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     print("Starting Asteroids!")
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60)/1000
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
