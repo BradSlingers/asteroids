34370f77efb043060413e1893f73fe5954440303
BradSlingers <btslingers@gmail.com>
2025-11-14 08:55:19 +0200

    got asteroids to appear and move

--- main.py
+++ main.py
@@ -2,23 +2,36 @@ import pygame
 from constants import *
 from logger import log_state
 from player import *
+from asteroid import *
+from asteroidfield import AsteroidField
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     print("Starting Asteroids!")
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
-    player = Player(x,y)
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    player = Player(x,y)
+    asteroidfield = AsteroidField()
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        #player.update(dt)
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
+        #player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
     print(f"Screen width: {SCREEN_WIDTH}")
