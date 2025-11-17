b973bd893cf748ef20737d4804c5c9c2f2c88472
BradSlingers <btslingers@gmail.com>
2025-11-17 10:04:09 +0200

    Got the bullets to fly

---
+++ shot.py
@@ -0,0 +1,14 @@
+from circleshape import *
+from constants import *
+from player import *
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        return pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+    
+    def update(self, dt):
+        self.position += self.velocity * dt
+        return self.position
